3015c466f2f53bc3278f0d85a83ecc161ce968b1
Desai <desainic@amazon.com>
2017-07-21 16:36:56 -0500

    Integrate microwaveeeeeee

--- handler.py
+++ handler.py
@@ -139,19 +139,6 @@ def build_speechlet_response(should_end_session, **kwargs):
 
     return response
 
-def build_simple_speechlet_response(title, output, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
-        },
-        'shouldEndSession': should_end_session
-    }
 
 def build_response(session_attributes, speechlet_response):
     return {
@@ -227,7 +214,7 @@ def build_suggestion(speech_output):
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     should_end_session = True
-    return build_response(session_attributes, build_simple_speechlet_response(
+    return build_response(session_attributes, build_speechlet_response(
             should_end_session,
             outputSpeech=speech_output))
             
